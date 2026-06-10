7fe4a53992bf877b5d5602348fd1fdc51208bf34
Chris <136855355+chrismarquezz@users.noreply.github.com>
2026-06-10 13:33:52 -0400

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        sub = Counter(s1)
+        window = Counter(s2[:len(s1)])
+        left = 0
+        right = len(s1)
+        while left < right and right < len(s2):
+            if sub == window:
+                return True
+            if s2[right] in window:
+                window[s2[right]] += 1
+            else:
+                window[s2[right]] = 1
+            window[s2[left]] -= 1
+            if window[s2[left]] == 0:
+                del window[s2[left]] 
+            right += 1
+            left += 1
+        return sub == window
+            
+            
